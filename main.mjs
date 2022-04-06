4a41216f1ff1fde4a41dd7ebe0eefa53960bb875
Leask Wong <i@leaskh.com>
2022-04-06 12:16:08 -0400

    removed debug code

--- main.mjs
+++ main.mjs
@@ -1,8 +1,8 @@
 import { socrates } from './index.mjs';
 
 await socrates.init({
-    user: 'leask',
-    password: 'nopassword',
-    httpOnly: true,
-    debug: true,
+    // user: 'leask',
+    // password: 'nopassword',
+    // httpOnly: true,
+    // debug: true,
 });
